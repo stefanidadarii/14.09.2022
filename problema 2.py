47e22e0925eeb950d44f921e4451efe673038a50
stefanidadarii <70660311+stefanidadarii@users.noreply.github.com>
2022-09-14 10:02:50 +0300

    Add files via upload

---
+++ problema 2.py
@@ -0,0 +1,17 @@
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+d=int(input('d='))
+
+def adunare():
+    s=a*d+b*c/c*b
+    return (s)
+
+def inmultire():
+    p=a*2/c*d
+    return(p)
+
+print(adunare(), inmultire())
+
+
+
