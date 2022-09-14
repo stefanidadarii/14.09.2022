85fd6d7272e07dc4b3340c5a4bb1ba271361d89c
stefanidadarii <70660311+stefanidadarii@users.noreply.github.com>
2022-09-14 20:59:41 +0300

    Add files via upload

---
+++ problema1.py
@@ -0,0 +1,10 @@
+def creare_lista():
+    n = int(input('Numarul de elemente: '))
+    lista_locala = []
+    for i in range(n):
+        element = eval(input(f'Elementul {i} este: '))
+        if type(element)==int:
+            lista_locala.append(element)
+    return lista_locala
+
+print(creare_lista())
